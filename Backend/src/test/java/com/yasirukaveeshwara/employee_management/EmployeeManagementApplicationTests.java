cac01dd9e3f08cd9b094e51e543fa3512ecab7ce
Yasiru Kaveeshwara <kaveeshwaray@gmail.com>
2025-04-20 11:23:22 +0530

    initialize project with springboot Backend

---
+++ Backend/src/test/java/com/yasirukaveeshwara/employee_management/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasirukaveeshwara.employee_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
